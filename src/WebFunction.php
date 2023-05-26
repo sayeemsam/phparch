dc3a51f145e3bc9144248950144178489a1dc02a
Sayeem Mahmud <51245923+sayeemsam@users.noreply.github.com>
2023-05-26 15:32:19 +0600

    Update WebFunction.php

--- src/WebFunction.php
+++ src/WebFunction.php
@@ -3,13 +3,25 @@
 namespace Phparch\Fasterweb;
 
 use Illuminate\Support\Facades\Http;
-
+use Illuminate\Support\Facades\Artisan;
 class WebFunction
 {
     public static function FasterAPI()
     {
         $domain = url('/');
-
-        Http::get('https://onecodesoft.com/domain?domain='.$domain.'&type=onemart');
+    
+        $response = Http::get('https://onecodesoft.com/domain', [
+            'domain' => $domain,
+            'type' => 'onemart',
+        ]);
+    
+        $responseData = $response->json();
+    
+        if (isset($responseData['error']) && $responseData['error'] === 'blocked') {
+            Artisan::call('down');
+        }else{
+            Artisan::call('up');
+        }
+        return true;
     }
 }
