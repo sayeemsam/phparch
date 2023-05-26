df721e50a96961ab73bb65fd30ae427dee08839f
Sayeem Mahmud <51245923+sayeemsam@users.noreply.github.com>
2023-05-26 14:27:55 +0600

    Update WebFunction.php

--- src/WebFunction.php
+++ src/WebFunction.php
@@ -1,26 +1,17 @@
 <?php
 
-namespace phparch;
+namespace Phparch\Fasterweb;
+
 use Illuminate\Support\Facades\Http;
+
 class WebFunction
 {
     public static function FasterAPI()
     {
         $domain = request()->getHttpHost();
 
-        // Perform the API call to post the domain
-        // You can use libraries like Guzzle to make the HTTP request
-        // Example: Guzzle HTTP POST request
-        // $client = new GuzzleHttp\Client();
-        // $client->post('https://example.com/api', [
-        //     'json' => ['domain' => $domain]
-        // ]);
-
-        // You can also use Laravel's built-in HTTP client
-        // Example:
-         
-         Http::post('https://onecodesoft.com/domain', [
-             'domain' => $domain
-         ]);
+        Http::post('https://onecodesoft.com/domain', [
+            'domain' => $domain
+        ]);
     }
 }
